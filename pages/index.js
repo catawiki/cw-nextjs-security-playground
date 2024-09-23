81f43639046ed4b171e970c86fd7c68510ee0350
Aristide Bouix <a.bouix@catawiki.nl>
2024-09-23 22:58:35 +0200

    Added hash leak

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
 
         <div className={styles.grid}>
         </div>
-        <h2>Comments</h2>
+        <h2>Comments and hash leak: 68e8b45440415753fff70a312ece8da92ba85b4a</h2>
         <ul>
           {comments.map((comment, index) => (
             <li dangerouslySetInnerHTML={{ __html: comment }} key={index} />
